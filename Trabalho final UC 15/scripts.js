62d2b76695bbdafb234b2cf251ced1239f87cf68
Jardielxp <jardielaureliano918@gmail.com>
2024-06-06 15:07:44 -0300

    a

--- Trabalho final UC 15/scripts.js
+++ Trabalho final UC 15/scripts.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var secretNumber = Math.floor(Math.random() * 11);
-    var attempts = 0;
-    var maxAttempts = 2;
+    var attempts = 1;
 
     document.getElementById('guessButton').addEventListener('click', function() {
         var userGuess = parseInt(document.getElementById('guessInput').value);
-        attempts++;
 
         if (isNaN(userGuess) || userGuess < 0 || userGuess > 10) {
             document.getElementById('message').innerText = 'Por favor, insira um número válido entre 0 e 10.';
@@ -16,18 +14,16 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('message').innerText = 'Parabéns! Você acertou o número!';
             document.getElementById('kuroko').src = "Imagens/Feliz.png";
             document.getElementById('kuroko').style.width = "80%";
-        } else if (attempts >= maxAttempts) {
+            document.getElementById('guessButton').setAttribute('disabled', true);
+        } else if (attempts == 0) {
             document.getElementById('message').innerText = 'Você perdeu! O número secreto era ' + secretNumber + '.';
             document.getElementById('kuroko').src = "Imagens/TristeFundo.png";
             document.getElementById('kuroko').style.width = "50%";
+            document.getElementById('guessButton').setAttribute('disabled', true);
         } else {
+            attempts--;
             document.getElementById('message').innerText = 'Tentativa incorreta. Tente novamente!';
 
-        }
-
-        if (attempts >= maxAttempts || userGuess === secretNumber) {
-            document.getElementById('guessButton').disabled = true;
-            document.getElementById('guessInput').disabled = true;
         }
     });
 
